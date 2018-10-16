83a42f9da010eb3cd3797c05a1a20d685e9e30cf
TomasNemecek <tomas@tomasnemecek.com>
2018-10-16 20:51:10 +0200

    Přidejte soubory projektu.

---
+++ Assignment4/model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment4.model
+{
+    [Table("categories")]
+    public class Category
+    {
+        [Column("categoryid")]
+        public int Id { get; set; }
+        [Column("categoryname")]
+        public string Name { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+    }
+}
---
+++ Assignment4/model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment4.model
+{
+    [Table("products")]
+    public class Product
+    {
+        [Column("productid")]
+        public int Id { get; set; }
+
+        [Column("productname")]
+        public int Name { get; set; }
+
+        [Column("CategoryId")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
